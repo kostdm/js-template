21fddd8a92d97adfee5177df88ad1c8240361ffd
kostdm <kostdm@gmail.com>
2020-01-04 12:26:32 +0500

    add bad code

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'cheap-eval-source-map',
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
