3382011f75acb27ba82794f616ab534656660821
kostdm <kostdm@gmail.com>
2020-01-09 23:45:24 +0500

    fix

--- src/articles.js
+++ src/articles.js
@@ -1,11 +1,41 @@
 export async function all(){
-
+    let data = await makeRequest(`http://localhost:8080/clients`);
+    return data;
 }
 
 export async function one(id){
-
+    let data = await makeRequest(`http://localhost:8080/clients/${id}`);
+    return data;
 }
 
 export async function remove(id){
+    let data = await makeRequest(`http://localhost:8080/clients/${id}`,{
+        method: 'delete'
+    });
+    return data;
+}
 
+export async function add(client){
+    let data = await makeRequest(`http://localhost:8080/clients`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(client)
+    });
+
+    return data;
+}
+
+function makeRequest(url, options = {}){
+    return fetch(url, options).then(response => {
+        if (response.status !== 200 && response.status !== 201) {
+            return response.text().then(text => {
+                throw new Error(text);
+            });
+        }
+
+        return response.json();
+    });
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,39 @@ import * as ArticlesModel from './articles';
     try {
         let articles = await ArticlesModel.all();
         console.log('articles count = ' + articles.length);
+
+        // // берём случайный индекс
+        // let ind = Math.floor(Math.random() * articles.length);
+        // console.log('select index ' + ind + ', id = ' + articles[ind]._id)
+        
+        // // получаем статью по id
+        // let article = await ArticlesModel.one(articles[ind]._id);
+        // console.log(article);
+
+        // // пробуем удалить её
+        // let res = await ArticlesModel.remove(article._id);
+        // console.log('что с удалением? - ' + res);
+
+        // let articlesNew = await ArticlesModel.all();
+        // console.log('articles count = ' + articlesNew.length);
+
+        // // создаем обратно
+        // let add = await ArticlesModel.add({
+        //     name: 'Ivan Ivanovich',
+        // });
+        // console.log('add client id: ' + add._id);
+
+        // let articlesNewest = await ArticlesModel.all();
+        // console.log('articles count = ' + articlesNewest.length);
+
+        // // а сколько статей в базе сейчас
+        // try {
+        //     articles = await ArticlesModel.all();
+        //     console.log('articles count = ' + articles.length);
+        // }
+        // catch(error) {
+        //     console.log(error + ' after delete');
+        // }
     }
     catch (error) {
         console.log(error);
